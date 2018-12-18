b9d91d7322aa0796ca46d48d80be149aef8b83df
Vicente Ferrari <v.ferrari01@ufromail.cl>
2018-12-18 03:10:09 -0300

    [fixed] indentacion de prueba unitaria

--- src/test/java/xyz/vferrari/maze/MazeTest.java
+++ src/test/java/xyz/vferrari/maze/MazeTest.java
@@ -10,20 +10,20 @@ import static org.junit.Assert.assertEquals;
 public class MazeTest {
 	@Test
 	public void mazeTest1() {
-		String input = "13\n" +
-				"2 2 4\n" +
-				"3 1 3 5\n" +
-				"2 2 4\n" +
-				"3 1 3 6\n" +
-				"2 2 6\n" +
-				"2 4 5\n" +
-				"2 8 9\n" +
-				"2 7 9\n" +
-				"2 7 8\n" +
-				"2 11 13\n" +
-				"2 10 12\n" +
-				"2 11 13\n" +
-				"2 10 12";
+		String input = 	"13\n" +
+						"2 2 4\n" +
+						"3 1 3 5\n" +
+						"2 2 4\n" +
+						"3 1 3 6\n" +
+						"2 2 6\n" +
+						"2 4 5\n" +
+						"2 8 9\n" +
+						"2 7 9\n" +
+						"2 7 8\n" +
+						"2 11 13\n" +
+						"2 10 12\n" +
+						"2 11 13\n" +
+						"2 10 12";
 
 		Maze maze = new Maze(input);
 		String actual = maze.toString();
